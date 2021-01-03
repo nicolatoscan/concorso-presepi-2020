9c08cf3bd60c6135e91a29659bba3b9669f8ff2a
nicolatoscan <nicolatoscan99@outlook.it>
2021-01-03 21:58:55 +0100

    added 2 presepi

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ export default defineComponent({
   },
   methods: {
     clearLocalStorage: function() {
-      localStorage.removeItem('ratings')
+      localStorage.removeItem('ratings2')
       location.reload()
     }
   }
